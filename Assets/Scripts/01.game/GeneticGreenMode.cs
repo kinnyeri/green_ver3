2d782ce82afc638232862ca9f61464922565225c
kinnyeri <98sena@naver.com>
2021-01-05 16:58:05 +0900

    atan2로 변경

--- Assets/Scripts/01.game/GeneticGreenMode.cs
+++ Assets/Scripts/01.game/GeneticGreenMode.cs
@@ -90,8 +90,8 @@ public class GeneticGreenMode : Progress
             stoppedPos = ball.transform.position;
             Debug.Log("Start Examine : " + stoppedPos.x + ", " + stoppedPos.z);
 
-            angle = Mathf.Atan((stoppedPos.z - startPos.transform.position.z) / (stoppedPos.x - startPos.transform.position.x))
-                - Mathf.Atan((hole.transform.position.z - startPos.transform.position.z) / (hole.transform.position.x - startPos.transform.position.x))*180/Mathf.PI;
+            angle = Mathf.Atan2((stoppedPos.z - startPos.transform.position.z), (stoppedPos.x - startPos.transform.position.x)) * Mathf.Rad2Deg
+                - Mathf.Atan2((hole.transform.position.z - startPos.transform.position.z), (hole.transform.position.x - startPos.transform.position.x)) * Mathf.Rad2Deg;
             Debug.Log("ANGLE " +angle); // 각도 계산이 잘 안됨
             //nextState();
         }
