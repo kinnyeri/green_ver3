b0a2ccc23e51ca1ae7b29f5c490281f9b122d6e4
Kim Yeri <98sena@naver.com>
2021-01-23 00:43:13 +0900

    거리 비율, 각도 저장

--- Assets/Scripts/01.game/GeneticGreenMode.cs
+++ Assets/Scripts/01.game/GeneticGreenMode.cs
@@ -25,7 +25,9 @@ public class GeneticGreenMode : Progress
     private int count = 0;
 
     float angle;
-
+    List<float> rightAngle;
+    List<float> distanceRate;
+    int countRound = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +37,9 @@ public class GeneticGreenMode : Progress
 
         hole.transform.position = new Vector3(random.Next(100, 200), 3.1f, random.Next(100, 200));
         startPos.transform.position = new Vector3(random.Next(100, 200), 8f, random.Next(100, 200));
+
+        rightAngle = new List<float>();
+        distanceRate  = new List<float>();
     }
 
     // Update is called once per frame
@@ -66,10 +71,11 @@ public class GeneticGreenMode : Progress
         {
             if (isNewStart)
             {
-                
+                //새 스타트 위치
                 ball.transform.position = startPos.transform.position;
+                //각도 랜덤 생성
+                abm.ChangeAngle(Random.Range(-180f,180f));
                 isNewStart = false;
-
             }
             cameraPos.transform.position = startCameraPos.transform.position;
             Debug.Log("Chaged Pos : " + ball.transform.position.x + ", " + ball.transform.position.z);
@@ -107,30 +113,38 @@ public class GeneticGreenMode : Progress
             
             stoppedPos = ball.transform.position;
             count++;
+            
             Debug.Log("count" + count);
             Debug.Log("Start Examine : " + stoppedPos.x + ", " + stoppedPos.z);
             // 각도 계산 
             angle = Mathf.Atan2((stoppedPos.z - startPos.transform.position.z), (stoppedPos.x - startPos.transform.position.x)) * Mathf.Rad2Deg
-                - Mathf.Atan2((hole.transform.position.z - startPos.transform.position.z), (hole.transform.position.x - startPos.transform.position.x)) * Mathf.Rad2Deg; 
+                - Mathf.Atan2((hole.transform.position.z - startPos.transform.position.z), (hole.transform.position.x - startPos.transform.position.x)) * Mathf.Rad2Deg;
+            rightAngle.Add(angle); // 각도 저장
+            //거리 비율 저장
+            distanceRate.Add(Mathf.Sqrt(Mathf.Pow(hole.transform.position.x, 2) + Mathf.Pow(hole.transform.position.z, 2)) / Mathf.Sqrt(Mathf.Pow(stoppedPos.x,2)+ Mathf.Pow(stoppedPos.z, 2)));
             Debug.Log("ANGLE " +angle);
+            Debug.Log("distanceRate " + distanceRate[countRound]);
+
+            countRound++;
             abm.finish = false; // 초기화
-            if (count >= 10||abm.succeed)
+            if (count >= 1 || abm.succeed) //count>=10이었다
             {
                 startPos.transform.position = new Vector3(random.Next(100, 200), 8f, random.Next(100, 200));
                 count = 0;
-                if(count==10) Debug.Log("10번 넘어감");
+                //if(count==10) Debug.Log("10번 넘어감");
                 if (abm.succeed)
                 {
+                    //홀에 들어갔을 경우
                     Debug.Log("들어감");
                     abm.succeed = false;
                 }
                 nextState();
             }
-            else if (!abm.succeed)
-            {
-                abm.ChangeAngle(angle);
-                nextState();
-            }
+            //else if (!abm.succeed)
+            //{
+            //    abm.ChangeAngle(angle);
+            //    nextState();
+            //}
         }
     }
     
